8bd29741434bbd96cb2080df4e83ed07f2c8af21
Ian Boyle <irboyle21@gmail.com>
2022-02-10 11:42:58 -0500

    adds get positions route

--- server/src/index.ts
+++ server/src/index.ts
@@ -20,12 +20,25 @@ app.use((req, res, next) => {
     next();
 });
 
+
+//ROUTES 
+// create position
 app.post('/positions', async (req: Request, res: Response) => {
   try {
       const { symbol, company_name, cost_basis, current_price, quantity  } = req.body;
       //$1 allows variables to be added to db?
       const newPosition = await pool.query('INSERT INTO positions (symbol, company_name, cost_basis, current_price, quantity) VALUES($1, $2, $3, $4, $5) RETURNING *', [symbol, company_name, cost_basis, current_price, quantity]);
       res.json(newPosition.rows[0]);
+  } catch (err: any) {
+      console.error(err.message);
+  }
+});
+
+// get all positions
+app.get('/positions', async (req: Request, res: Response) => {
+  try {
+      const allPositions = await pool.query('SELECT * FROM positions');
+      res.json(allPositions.rows);
   } catch (err: any) {
       console.error(err.message);
   }
